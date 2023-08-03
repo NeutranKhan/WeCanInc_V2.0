3894a4d6c400948104930609c94d9321e821a122
Michael Khan <137360920+NeutranKhan@users.noreply.github.com>
2023-08-04 00:14:49 +0100

    Update settings.py

--- wecaninc/settings.py
+++ wecaninc/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+ 
 
 SECRET_KEY = 'we-can-inc-qi47yq%b=@*6ymv(@M13i9c3h8a1e5l12)w-a5a^^2brh!2drxqr8gfvvu'
 
